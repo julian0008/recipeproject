839b367458f4d7009d0a05be342038bee8a8c809
JulianAmin <jamin@gofluent.com>
2019-03-20 18:21:00 +0800

    added logging in bootstrap and controller

--- src/main/java/com/springframework/recipeproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/springframework/recipeproject/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import com.springframework.recipeproject.domain.*;
 import com.springframework.recipeproject.repositories.CategoryRepository;
 import com.springframework.recipeproject.repositories.RecipeRepository;
 import com.springframework.recipeproject.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
@@ -27,7 +29,8 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-         recipeRepository.saveAll(getRecipes());
+        log.debug("in bootstrap class...");
+        recipeRepository.saveAll(getRecipes());
     }
 
     private List<Recipe> getRecipes() {
